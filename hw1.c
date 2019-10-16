ec39e6e220517cbe1076bb7fc253df11933a5486
Chris Allen <allenc5@rpi.edu>
2019-10-15 20:01:48 -0400

    Regex is now compiled, supports groups, and checks all recursion branches for matches

--- hw1.c
+++ hw1.c
@@ -6,152 +6,264 @@
 
 #define MAX_LENGTH 257
 
-/* see if character matches a meta escape character */
-bool match_slash(const char cur, const char token)
+enum meta_char
 {
-    switch (token)
-    {
-    case 'd':
-    {
-        return isdigit(cur) == 1;
-    }
-    case 'D':
-    {
-        return isdigit(cur) == 0;
-    }
-    case 'w':
-    {
-        return isalpha(cur) == 1;
-    }
-    case 'W':
-    {
-        return isalpha(cur) == 0;
-    }
-    case 's':
-    {
-        return cur == ' ' || cur == '\t';
-    }
-    default:
-    {
-        return token == '\\';
-    }
-    }
-}
+    DOT,
+    PLUS,
+    STAR,
+    QMARK,
+    DIGIT,
+    NON_DIGIT,
+    LETTER,
+    NON_LETTER,
+    SPACE,
+    GROUP,
+    COMP_GROUP,
+    END
+};
 
-/*Determines if next character in match_star matches next char in reg_pattern*/
-bool matches_stop(const char *reg_pattern, int pattern_idx, const char cur)
+typedef struct reg_char
 {
-    if (reg_pattern[pattern_idx] == '\\')
-        return match_slash(cur, reg_pattern[pattern_idx + 1]);
-    else
+    bool is_meta;
+    char match_char;
+    enum meta_char meta;
+    struct reg_char *group;
+} reg_char;
+
+bool match_reg(const struct reg_char reg, const char c)
+{
+    if (reg.is_meta)
     {
-        // If stop char is optional, look for next stop char
-        if (reg_pattern[pattern_idx + 1] == '*' ||
-            reg_pattern[pattern_idx + 1] == '?')
+        switch (reg.meta)
         {
-            if (reg_pattern[pattern_idx + 2] == '\0' ||
-                reg_pattern[pattern_idx + 2] == '\n')
-                return true;
-            return matches_stop(reg_pattern, pattern_idx + 2, cur);
-        }
-        return cur == reg_pattern[pattern_idx] ||
-               reg_pattern[pattern_idx] == '.';
-    }
-}
-
-/* Used for + and * to match as many chars as possible */
-int match_star(const char *line, const char *reg_pattern, int line_idx,
-               int pattern_idx, bool is_slash)
-{
-    // Highest index of char that matches next in regex pattern
-    int stop_index = -1;
-    char token = reg_pattern[pattern_idx];
-    while ((((token == '.' || token == line[line_idx]) && !is_slash) ||
-            match_slash(line[line_idx], token)) &&
-           line[line_idx] != '\n' && line[line_idx] != '\0')
-    {
-        // If this matches char after * or + save index
-        if (matches_stop(reg_pattern, pattern_idx + 2, line[line_idx]))
-            stop_index = line_idx;
-        line_idx++;
-    }
-    return stop_index != -1 ? stop_index : line_idx;
-}
-
-/* Determine if current char in line matches with current regex char */
-bool match_char(const char *line, const char *reg_pattern, int *line_idx,
-                int pattern_idx)
-{
-    char cur = line[*line_idx];
-    char token = reg_pattern[pattern_idx];
-    bool is_slash = pattern_idx > 0 && reg_pattern[pattern_idx - 1] == '\\';
-    if (token == '\\')
-        return true;
-    if (reg_pattern[pattern_idx + 1] == '*')
-    {
-        *line_idx = match_star(line, reg_pattern, *line_idx, pattern_idx,
-                               is_slash);
-        return true;
-    }
-    if (reg_pattern[pattern_idx + 1] == '+')
-    {
-        int new_index = match_star(line, reg_pattern, *line_idx, pattern_idx,
-                                   is_slash);
-        // If index has not increased, then at least one match was not found
-        //printf("line index: %d\n new index: %d", *line_idx, new_index);
-        if (new_index == *line_idx)
+        case DOT:
+            return true;
+        case DIGIT:
+            return isdigit(c) != 0;
+        case NON_DIGIT:
+            return isdigit(c) == 0;
+        case LETTER:
+            return isalpha(c) != 0;
+        case NON_LETTER:
+            return isalpha(c) == 0;
+        case SPACE:
+            return c == ' ' || c == '\t';
+        case GROUP:
+        {
+            int i = 0;
+            while (reg.group[i].meta != END)
+            {
+                if (match_reg(reg.group[i], c))
+                    return true;
+                i++;
+            }
             return false;
-        *line_idx = new_index;
-        return true;
-    }
-    if (is_slash)
-    {
-        *line_idx = *line_idx + 1;
-        bool res = match_slash(cur, token);
-        return res;
-    }
-    if (reg_pattern[pattern_idx + 1] == '?')
-        return true;
-    if (token == '*' || token == '+' || token == '?')
-        return true;
-    if (token == cur || token == '.')
-    {
-        *line_idx = *line_idx + 1;
-        return true;
-    }
-    return false;
-}
-
-/* Recursively match the pattern */
-int match_pattern(const char *line, const char *reg_pattern,
-                  int line_idx, int pattern_idx, int starting_pos)
-{
-    // The end of the pattern was reached, so there was a successful match
-    if (reg_pattern[pattern_idx] == '\0' || reg_pattern[pattern_idx] == '\n')
-    {
-        return starting_pos;
-    }
-    // The end of the line was reached without finding a match
-    if (line[line_idx] == '\0' && reg_pattern[pattern_idx] != '?' &&
-        reg_pattern[pattern_idx] != '*' && reg_pattern[pattern_idx] != '+')
-    {
-        return -1;
-    }
-    // If current char matches, recursively check for next match
-    if (match_char(line, reg_pattern, &line_idx, pattern_idx))
-    {
-        return match_pattern(line, reg_pattern, line_idx, pattern_idx + 1,
-                             starting_pos);
+        }
+        case COMP_GROUP:
+        {
+            int i = 0;
+            while (reg.group[i].meta != END)
+            {
+                if (match_reg(reg.group[i], c))
+                    return false;
+                i++;
+            }
+            return true;
+        }
+        default:
+            return false;
+        }
     }
     else
     {
-        return match_pattern(line, reg_pattern, starting_pos + 1, 0,
-                             starting_pos + 1);
+        return c == reg.match_char;
+    }
+}
+
+reg_char *compile_reg(const char *reg_pattern)
+{
+    reg_char *pattern = malloc(MAX_LENGTH);
+    int i = 0, j = 0;
+
+    while (reg_pattern[i] != '\n' && reg_pattern[i] != '\0')
+    {
+        switch (reg_pattern[i])
+        {
+        case '.':
+        {
+            pattern[j].is_meta = true;
+            pattern[j].meta = DOT;
+        }
+        break;
+        case '+':
+        {
+            pattern[j].is_meta = true;
+            pattern[j].meta = PLUS;
+        }
+        break;
+        case '*':
+        {
+            pattern[j].is_meta = true;
+            pattern[j].meta = STAR;
+        }
+        break;
+        case '?':
+        {
+            pattern[j].is_meta = true;
+            pattern[j].meta = QMARK;
+        }
+        break;
+        case '\\':
+        {
+            i += 1;
+            switch (reg_pattern[i])
+            {
+            case 'd':
+            {
+                pattern[j].is_meta = true;
+                pattern[j].meta = DIGIT;
+            }
+            break;
+            case 'D':
+            {
+                pattern[j].is_meta = true;
+                pattern[j].meta = NON_DIGIT;
+            }
+            break;
+            case 'w':
+            {
+                pattern[j].is_meta = true;
+                pattern[j].meta = LETTER;
+            }
+            break;
+            case 'W':
+            {
+                pattern[j].is_meta = true;
+                pattern[j].meta = NON_LETTER;
+            }
+            case 's':
+            {
+                pattern[j].is_meta = true;
+                pattern[j].meta = SPACE;
+            }
+            break;
+            default:
+            {
+                pattern[j].is_meta = false;
+                pattern[j].match_char = '\\';
+            }
+            break;
+            }
+        }
+        break;
+        case '[':
+        {
+            pattern[j].is_meta = true;
+            if (reg_pattern[i + 1] == '^')
+            {
+                pattern[j].meta = COMP_GROUP;
+                i++;
+            }
+            else
+            {
+                pattern[j].meta = GROUP;
+            }
+            char reg_group[MAX_LENGTH];
+            int k = 0;
+            i++;
+            while (reg_pattern[i] != ']')
+            {
+                reg_group[k] = reg_pattern[i];
+                i++;
+                k++;
+            }
+            pattern[j].group = compile_reg(reg_group);
+        }
+        break;
+        default:
+        {
+            pattern[j].is_meta = false;
+            pattern[j].match_char = reg_pattern[i];
+        }
+        break;
+        }
+        i++;
+        j++;
+    }
+    pattern[j].meta = END;
+    return pattern;
+}
+
+bool next_match(const char cur, const reg_char *pattern, int pattern_idx)
+{
+    reg_char cur_reg = pattern[pattern_idx];
+    if (cur_reg.meta == END)
+        return false;
+    else if (match_reg(cur_reg, cur))
+        return true;
+    else if (pattern[pattern_idx + 1].meta == STAR ||
+             pattern[pattern_idx + 1].meta == QMARK)
+        return next_match(cur, pattern, pattern_idx + 2);
+    else
+        return false;
+}
+
+int match_p(const char *line, const reg_char *pattern, int line_idx,
+            int pattern_idx, int starting_pos)
+{
+    if (pattern[pattern_idx].meta == END)
+        return starting_pos;
+    if (line[line_idx] == '\0' || line[line_idx] == '\n')
+        return -1;
+    else
+    {
+        reg_char cur = pattern[pattern_idx];
+        reg_char next = pattern[pattern_idx + 1];
+        if (next.meta == STAR || next.meta == PLUS)
+        {
+            if (next.meta == PLUS)
+            {
+                if (!match_reg(cur, line[line_idx]))
+                    return match_p(line, pattern, starting_pos + 1, 0,
+                                   starting_pos + 1);
+                line_idx++;
+            }
+            while (match_reg(pattern[pattern_idx], line[line_idx]) &&
+                   line[line_idx] != '\0' && line[line_idx] != '\n')
+            {
+                if (next_match(line[line_idx], pattern, pattern_idx + 2))
+                {
+                    if (match_p(line, pattern, line_idx, pattern_idx + 2, starting_pos) != -1)
+                        return starting_pos;
+                }
+                line_idx += 1;
+            }
+            return match_p(line, pattern, line_idx, pattern_idx + 2,
+                           starting_pos);
+        }
+        else if (next.meta == QMARK)
+        {
+            if (match_reg(cur, line[line_idx]))
+            {
+                if (match_p(line, pattern, line_idx + 1, pattern_idx + 2,
+                            starting_pos) != -1)
+                    return starting_pos;
+            }
+            return match_p(line, pattern, line_idx, pattern_idx + 2,
+                           starting_pos);
+        }
+        else
+        {
+            if (match_reg(cur, line[line_idx]))
+                return match_p(line, pattern, line_idx + 1, pattern_idx + 1,
+                               starting_pos);
+            return match_p(line, pattern, starting_pos + 1, 0, starting_pos + 1);
+        }
     }
 }
 
 /* Read regex into string */
-void read_regex(const char *re_file, char *reg_pattern)
+reg_char *read_regex(const char *re_file)
 {
     FILE *regex_file = fopen(re_file, "r");
     if (regex_file == NULL)
@@ -159,12 +271,14 @@ void read_regex(const char *re_file, char *reg_pattern)
         fprintf(stderr, "ERROR: regex file <%s> does not exist\n", re_file);
         exit(1);
     }
+    char reg_pattern[MAX_LENGTH];
     fgets(reg_pattern, MAX_LENGTH, regex_file);
     fclose(regex_file);
+    return compile_reg(reg_pattern);
 }
 
 /* Read the file while matching each line to regex pattern */
-void parse_file(const char *in_file, char *reg_pattern)
+void parse_file(const char *in_file, reg_char *pattern)
 {
     FILE *input_file = fopen(in_file, "r");
     if (input_file == NULL)
@@ -175,7 +289,7 @@ void parse_file(const char *in_file, char *reg_pattern)
     char line[MAX_LENGTH];
     while (fgets(line, MAX_LENGTH, input_file))
     {
-        if (match_pattern(line, reg_pattern, 0, 0, 0) != -1)
+        if (match_p(line, pattern, 0, 0, 0) != -1)
         {
             // Append newline char if the line does not have one
             if (line[strlen(line) - 1] == '\n')
@@ -191,14 +305,6 @@ void parse_file(const char *in_file, char *reg_pattern)
     fclose(input_file);
 }
 
-/* Initailize regex pattern and then match */
-void perform_grep(char *re_file, char *in_file)
-{
-    char reg_pattern[MAX_LENGTH];
-    read_regex(re_file, reg_pattern);
-    parse_file(in_file, reg_pattern);
-}
-
 int main(int argc, char **argv)
 {
     setvbuf(stdout, NULL, _IONBF, 0);
@@ -208,5 +314,5 @@ int main(int argc, char **argv)
                         "USAGE: a.out <regex-file> <input-file>\n");
         return 1;
     }
-    perform_grep(argv[1], argv[2]);
+    parse_file(argv[2], read_regex(argv[1]));
 }
